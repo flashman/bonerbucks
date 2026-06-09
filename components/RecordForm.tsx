713433f0da8516462b40103859e44eb4d7903353
flashman <michael.flashman@gmail.com>
2026-06-09 14:31:07 -0700

    Remove arbitrary maxHeight on image preview

--- components/RecordForm.tsx
+++ components/RecordForm.tsx
@@ -371,7 +371,7 @@ export default function RecordForm({ initialSerial = "", record, redirectTo }: P
           <div style={{ marginTop: 10 }}>
             <div className="relative group" style={{ maxWidth: 500 }}>
               {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img src={previewUrl} alt="Preview" onClick={() => setLightbox(true)} style={{ maxWidth: "100%", maxHeight: 300, height: "auto", display: "block", cursor: "pointer" }} />
+              <img src={previewUrl} alt="Preview" onClick={() => setLightbox(true)} style={{ maxWidth: "100%", height: "auto", display: "block", cursor: "pointer" }} />
               <div className="absolute top-1 left-1 flex gap-1 opacity-0 group-hover:opacity-100 [@media(hover:none)]:opacity-100 transition-opacity duration-150">
                 {(["rotateCCW", "rotateCW", "flipH"] as const).map((t) => (
                   <button key={t} type="button" aria-label={{ rotateCCW: "Rotate left", rotateCW: "Rotate right", flipH: "Flip" }[t]} disabled={scanning} onClick={() => applyTransform(t)} style={{ background: "rgba(0,0,0,0.55)", color: "white", border: "none", cursor: scanning ? "default" : "pointer", width: 26, height: 26, fontSize: 14, display: "flex", alignItems: "center", justifyContent: "center" }}>
